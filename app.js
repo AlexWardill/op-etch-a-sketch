84525a9494d554a5195571cc5791ee38bfde26c1
Alex Wardill <alexander@wardill.net>
2021-08-01 17:40:42 +0100

    added basic vars in js

---
+++ app.js
@@ -0,0 +1,7 @@
+const body = document.getElementsByTagName('body');
+const h1 = document.getElementsByTagName('h1');
+const button = document.getElementsByTagName('button');
+const container = document.getElementById('container');
+
+// make 16x16 grid inside container
+
