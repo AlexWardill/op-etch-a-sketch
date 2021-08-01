a22a570e32e42d80b2ef3a292aa90bfb69676026
Alex Wardill <alexander@wardill.net>
2021-08-01 18:31:22 +0100

    Changed from float to grid. Added grid creation function

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const h1 = document.getElementsByTagName('h1');
 const button = document.getElementsByTagName('button');
 const container = document.getElementById('container');
 
-// make 16x16 grid inside container
+// Method of creating grid size
+function createGrid(gridSize) {
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
-for (let i=0; i<256; i++) {
-    var div = document.createElement('div');
-    div.id = `${i}`;
-    div.setAttribute('class', 'box random-hover')
+    for (let i=0; i<gridSize*gridSize; i++) {
+        var div = document.createElement('div');
+        div.id = `${i}`;
+        div.setAttribute('class', 'box random-hover');
+    
+        container.appendChild(div);
+    }
 
-    container.appendChild(div);
 }
+
+createGrid(10); // default grid size
+
+
