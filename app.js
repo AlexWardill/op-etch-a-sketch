7838d90f377a73947d83841bb16d340c8e34877b
Alex Wardill <alexander@wardill.net>
2021-08-01 19:27:22 +0100

    random color on mouseover. Issues with reset grid - doesn't reset colors of all squares

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const h1 = document.getElementById('h1');
 const button = document.getElementById('button');
 const container = document.getElementById('container');
 
+// random color
+function randomColor() {
+    let randomR = Math.floor(Math.random()*255);
+    let randomG = Math.floor(Math.random()*255);
+    let randomB = Math.floor(Math.random()*255);
+    
+    return `rgb(${randomR}, ${randomG}, ${randomB})`;
+}
 
+// create grid
 function createGrid(gridSize) {
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
@@ -11,13 +20,20 @@ function createGrid(gridSize) {
     for (let i=0; i<gridSize*gridSize; i++) {
         var div = document.createElement('div');
         div.id = `${i}`;
-        div.setAttribute('class', 'box random-hover');
+        div.setAttribute('class', 'box');
     
         container.appendChild(div);
     }
+    // add event listener for mouseover, change background colour to random colour
+    let boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', function changeColor() {
+            this.style.background = randomColor();
+        });
+    });
 }
 
-
+// reset grid
 function resetGrid() {
     let newSize = prompt("Enter new grid size: ");
     return createGrid(newSize);
@@ -25,5 +41,9 @@ function resetGrid() {
 
 button.addEventListener('click', resetGrid);
 
+//
 
 createGrid(10); // default grid size
+
+
+
